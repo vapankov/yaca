93caeee5917a95094ec09daf350648f3bde72bb1
Vladimir Pankov <243214204+vapankov@users.noreply.github.com>
2026-06-28 15:00:03 +0300

    Add simple cli for messaging.

---
+++ cmd/cli/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/vapankov/yaca/internal/domains/messaging/infrastructure/adapters/inbound/cli"
+)
+
+func main() {
+	cli.Run()
+}
---
+++ internal/domains/messaging/infrastructure/adapters/inbound/cli/cli.go
@@ -0,0 +1,98 @@
+package cli
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/vapankov/yaca/internal/domains/messaging/application/usecases"
+	"github.com/vapankov/yaca/internal/domains/messaging/core/values"
+	"github.com/vapankov/yaca/internal/domains/messaging/infrastructure/adapters/outbound/clock"
+	idGen "github.com/vapankov/yaca/internal/domains/messaging/infrastructure/adapters/outbound/generators/identifiers"
+	messageRepoFileStore "github.com/vapankov/yaca/internal/domains/messaging/infrastructure/adapters/outbound/repositories/message/filestore"
+	fileStorage "github.com/vapankov/yaca/internal/domains/messaging/infrastructure/storage/file"
+)
+
+const (
+	messagingCommandPost = "post"
+	messagingCommandView = "view"
+)
+
+func Run() {
+	defer func() {
+		fmt.Println("QUIT")
+	}()
+
+	const (
+		messageStoreFileDefault = "messages"
+		messagingCommandDefault = messagingCommandView
+	)
+
+	var (
+		messageStoreFile string
+		messagingCommand string
+		messagingData    string
+	)
+
+	flag.StringVar(&messageStoreFile, "file", messageStoreFileDefault, "Use storage file.")
+	flag.StringVar(&messagingCommand, "cmd", messagingCommandDefault, "Command to run: \"post\" or \"view\".")
+	flag.StringVar(&messagingData, "data", "", "Message to send.")
+
+	flag.Parse()
+
+	var (
+		fileLineStore     = fileStorage.NewLineStore(messageStoreFile)
+		messageRepository = messageRepoFileStore.New(fileLineStore)
+		messsageIDGen     = idGen.NewMessage()
+		clock             = clock.New()
+
+		messagingUsecases = usecases.New(
+			messageRepository,
+			messsageIDGen,
+			clock,
+		)
+	)
+
+	switch messagingCommand {
+	case messagingCommandPost:
+		if len(messagingData) == 0 {
+			fmt.Println("EMPTY MESSAGE")
+			return
+		}
+
+		if _, err := messagingUsecases.PostMessage(context.TODO(), &usecases.PostMessageParams{
+			MessageContents: values.MessageContents(messagingData),
+		}); err != nil {
+			fmt.Println("ERROR", err.Error())
+			return
+		}
+
+		fmt.Println("MESSAGE SENT")
+
+	case messagingCommandView:
+		result, err := messagingUsecases.ViewMessages(context.TODO(), &usecases.ViewMessagesParams{})
+		if err != nil {
+			fmt.Println("ERROR", err.Error())
+			return
+		}
+
+		if len(result.Items) == 0 {
+			fmt.Println("NO MESSAGES")
+			return
+		}
+
+		for i := range result.Items {
+			printMessage(&result.Items[i])
+		}
+	}
+
+}
+
+func printMessage(msg *usecases.ViewMessagesResultItem) {
+	fmt.Println(">>>")
+	fmt.Println("ID\t", msg.MessageID)
+	fmt.Println("DATA\t", msg.MessageContents)
+	fmt.Println("TS\t", time.Time(msg.MessageCreatedAt).Local().Format(time.RFC3339))
+	fmt.Println("<<<")
+}
